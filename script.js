11ff73a41f07ad0cb5d7c5db5f3b49d571fd23b3
MRITUNJAY <47263413+jay229@users.noreply.github.com>
2023-07-31 21:31:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let str=prompt("Enter a string")
+let map=new Map();
+for(let i=0;i<str.length;i++){
+	let freq=1;
+	if(map.has(str[i])){
+		freq=map.get(str[i]);
+		freq+=1;
+	}
+	map.set(str[i],freq)
+}
+let ans;
+map.forEach((value,key)=>{
+	if (value==1) {
+		ans=key;
+		break;
+	}
+})
+console.log(ans)
+
